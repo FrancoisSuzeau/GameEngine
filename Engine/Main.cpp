0eedf518f6399fd9f0fbd16df0cc81719cc76dbf
FrancoisSuzeau <frsuzeau@gmail.com>
2023-05-28 15:46:39 +0200

    get the necessary parameter for the window

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -3,6 +3,8 @@
 // Purpose : Program start
 /******************************************************************************************************************************************/
 #include <iostream>
+#include <memory>
+
 #include "Services/GraphicInitializerService.hpp"
 #include "Services/AudioInitializerService.hpp"
 #include "Director.hpp"
@@ -17,6 +19,12 @@ int main(int argc, char** argv)
     dir.SetBuilder<Services::AudioInitializerService>();
 
     dir.StartAllBuilder();
+
+    IoC::Container::Container* container = IoC::Container::Container::GetInstanceContainer();
+    std::shared_ptr<Services::GraphicInitializerService> graph_service_init = container->make<Services::GraphicInitializerService>();
+
+    SDL_GLContext gl_context = graph_service_init->GetGLContext();
+    SDL_Window* sdl_window = graph_service_init->GetSDLWindow();
 
     dir.EndingBuilder();
 
