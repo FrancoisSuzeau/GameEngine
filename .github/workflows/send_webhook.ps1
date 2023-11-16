05e0dacf70cd4d71ae8286006ecea565e305722c
François <frsuzeau@gmail.com>
2023-11-16 20:45:39 +0100

    Remove json  file

--- .github/workflows/send_webhook.ps1
+++ .github/workflows/send_webhook.ps1
@@ -1,7 +1,7 @@
 $url = "https://discord.com/api/webhooks/1171550224045576224/fWJf83DLoXugzWao-KxdEq-NwfgyCh0fmEA4Jdwtqdwomke5c5o17Vne7o90M6FYWxf6"
 
 $jsonBody = @{
-    content = "Hello"
+    content = "A new build is available"
 } | ConvertTo-Json
 
 Invoke-RestMethod -Uri $url -Method Post -Headers @{"Content-Type" = "application/json"} -Body $jsonBody
