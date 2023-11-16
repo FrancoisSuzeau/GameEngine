0192a519b145bf70bd9bfd7e965d2693135bb4ef
François <frsuzeau@gmail.com>
2023-11-16 20:41:10 +0100

    made some rework

--- .github/workflows/send_webhook.ps1
+++ .github/workflows/send_webhook.ps1
@@ -1,8 +1,8 @@
 $url = "https://discord.com/api/webhooks/1171550224045576224/fWJf83DLoXugzWao-KxdEq-NwfgyCh0fmEA4Jdwtqdwomke5c5o17Vne7o90M6FYWxf6"
 
-$jsonFilePath = "data.json"
-
-$jsonBody = Get-Content -Path $jsonFilePath | Out-String | ConvertFrom-Json | ConvertTo-Json
+$jsonBody = @{
+    content = "Hello"
+} | ConvertTo-Json
 
 Invoke-RestMethod -Uri $url -Method Post -Headers @{"Content-Type" = "application/json"} -Body $jsonBody
 
