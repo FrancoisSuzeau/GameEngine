f62df85642aba6e6af3afcb4fe157757c75e384e
François <frsuzeau@gmail.com>
2024-02-15 21:05:21 +0100

    cattttt

--- .github/workflows/send_webhook_pr.ps1
+++ .github/workflows/send_webhook_pr.ps1
@@ -60,7 +60,7 @@ $payload = [PSCustomObject]@{
 } | ConvertTo-Json -Depth 4
 
 try {
-    $payload | http $WEBHOOK_URL
+    '{"username": "Cat", "content": "meow", "embeds": [{"title": "Cool!"}]}' | http $WEBHOOK_URL
 }
 catch {
     Write-Host "Error: $_"
