493de20c4a120571c8507512af3a003a2cc783e0
François <frsuzeau@gmail.com>
2024-02-15 20:23:27 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- .github/workflows/send_webhook_pr.ps1
+++ .github/workflows/send_webhook_pr.ps1
@@ -58,14 +58,6 @@ $payload = [PSCustomObject]@{
     content = $webhook_content
     embeds = $embedArray.ToArray()
 } 
-#Sending the webhook
-# try {
-#     $response = Invoke-RestMethod -Uri $discordWebhookUrl -Method Post -Body ($payload | ConvertTo-Json) -Headers @{"Content-Type" = "application/json"} 
-#     $response
-# }
-# catch {
-#     Write-Host "Error: $_"
-# }
 
 
 try {
